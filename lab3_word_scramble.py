9b0977293ef68c80acfe9547f2428bac80b6d4ae
kathlyn4847 <kathlynoneill@gmail.com>
2019-10-15 17:40:47 +0100

    lab3

--- lab3_word_scramble.py
+++ lab3_word_scramble.py
@@ -7,12 +7,52 @@
 import string
 
 class WordScramble:
+    #__init__ functions show up whether called or not.
     def __init__(self):
         self.user_input = input("Please give me a sentence: ")
 
     def scramble(self):
         # print what was input
-        print("The user input was: ", self.user_input)
+        print("The user input was: ", str(self.user_input))
+
+        if len(self.user_input) > 3:
+            #swap first two indexes.
+            new_word = self.user_input[0] + self.user_input[2] + self.user_input[1] + self.user_input[3:]
+        elif len(self.user_input) <= 3:
+            new_word = self.user_input
+        else:
+            #new_word doesn't exist yet for the print() below so
+            #create the variable for when it falls in the else.
+            print("try again")
+            new_word = False
+
+        print(new_word)
+
+        sentence = self.user_input.strip().split()
+        #strip() takes out whitespaces.
+        for index, word in enumerate(sentence):
+            if len(word) > 3:
+                #container to swap letters
+                temp_word = list(word)
+
+                if(',' in temp_word):
+                    #swap but keep positionn of comma ,
+                    temp = temp_word[1]
+                    temp_word[1] = temp_word[-3]
+                    temp_word[-3] = temp
+
+                else:
+                    #normal swap
+                    temp = temp_word[1]
+                    temp_word[1] = temp_word[2]
+                    temp_word[2] = temp
+
+                temp_word = ''.join(temp_word)
+                #put the word into
+                sentence[index] = temp_word
+            else:
+                #new word is same as the input in else
+                sentence[index] = word
 
         # first scramble is just one word
         # reverse two indices
@@ -22,9 +62,10 @@ class WordScramble:
 
 
         # now try to scramble one sentence
+
+
         # do just words first, then you can move on to work on
         # punctuation
 
 word_scrambler = WordScramble()
 word_scrambler.scramble()
-
